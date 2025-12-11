7b30c9ff778a747e1e90593465141e5592edc350
dario <dario.bassi111@gmail.com>
2025-12-11 21:15:30 +0100

    deleted implemenation file from repo

--- devices_camera.py
+++
@@ -1,6 +0,0 @@
-from registry import device_registry
-from pymmcore_camera_sim import SimCameraDevice
-
-@device_registry("CameraDevice")
-def make_camera(core, microscope_simulation):
-    return SimCameraDevice(core, microscope_simulation)
